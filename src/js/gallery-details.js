922a387f83fd77a7cd0e8f01522c2421a77e4125
impact0823@naver.com <young1596>
2017-03-02 15:22:27 +0900

    transform : list-limit-moving && resize

--- src/js/gallery-details.js
+++ src/js/gallery-details.js
@@ -1,32 +1,102 @@
 require([
 	"common",
 ], function() {
-	var slideIndex =1;
-	$(".arrow-right").on("click", function() {
+	var slider = {
+		index: 0,
+		tx: [],
+	};
+
+	$(window).on("resize", function() {
 		var listWidth = $(".show-viewer-lists>li").width() +10;
-		$(".show-viewer-lists>li").animate({
-			left: 100,
-		}, {
-			duration: 1000,
-			step: function(now) {
-				$(this).css("transform", "translate(-"+listWidth *slideIndex* (now/100.00)+"px)");
-			},
-		}).promise().done(function() {
-			$(".show-viewer-lists>li").css("left", listWidth *slideIndex+"px");
-			slideIndex++;
-		});
+		var listCount = $(".show-viewer-lists>li").length;
+
+		var nowTx = listWidth * -slider.index;
+
+		for (var count=0; count<listCount; count++) {
+			slider.tx[count] = nowTx;
+		}
+
+		$(".show-viewer-lists>li").css("transform", "translateX(" + nowTx + "px)");
 	});
 
-	$(".arrow-left").on("click", function() {
+	$(".arrow-left, .arrow-right").on("click", function() {
+		/* 애니메이션 도중에 실행 안되게 하는 처리*/
+		if (slider.animating) {
+			return;
+		}
+
+		var listWidth = $(".show-viewer-lists>li").width() +10;
+		var listCount = $(".show-viewer-lists>li").length;
+		var maxIndex = -listWidth *(listCount -1);
+
+		slider.animating = true;
+		slider.direction = $(this).hasClass("arrow-left") ? "left" : "right";
+
 		$(".show-viewer-lists>li").animate({
 			left: 100,
 		}, {
 			duration: 1000,
+			start: function() {
+				slider.tx[$(this).index()] = slider.tx[$(this).index()] || 0;
+			},
 			step: function(now) {
-				var listWidth = $(this).width() +10;
-				$(this).css("transform", "translateX(+"+listWidth *slideIndex* (now/100.00)+"px)");
+				var tx = slider.tx[$(this).index()];
+				var move = listWidth * (now/100.00);
+
+				if (slider.direction === "left") {
+					if (tx >= 0) {
+						tx = 0;
+					}
+					else {
+						tx += move;
+					}
+				}
+				else {
+					if (tx <= maxIndex) {
+						tx = maxIndex;
+					}
+					else {
+						tx -= move;
+					}
+				}
+
+				$(this).css("transform", "translateX("+ tx +"px)");
+			},
+			complete: function() {
+				$(this).css("left", 0);
+				if (slider.direction === "left") {
+					if (slider.tx[$(this).index()] >= 0) {
+						slider.tx[$(this).index()]=0;
+						if ($(this).index() === 0) {
+							slider.index = 0;
+						}
+					}
+					else {
+						if ($(this).index() === 0) {
+							slider.index--;
+							console.log("왼쪽"+slider.index);
+						}
+						slider.tx[$(this).index()] += listWidth;
+					}
+				}
+				else {
+					if (slider.tx[$(this).index()] <= maxIndex) {
+						slider.tx[$(this).index()] = maxIndex;
+						if ($(this).index() === 0) {
+							slider.index =listCount-1;
+						}
+					}
+					else {
+						if ($(this).index() === 0) {
+							slider.index++;
+							console.log("오른쪽"+slider.index);
+						}
+						slider.tx[$(this).index()] -= listWidth;
+					}
+				}
+
+				slider.animating = false;
 			},
 		});
-		slideIndex--;
 	});
 });
